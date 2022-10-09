91d4ec6699131cd603164eeffdaee5ab829933cd
RenanFerreira0412 <renan_igor03@hotmail.com>
2022-10-09 13:20:06 -0300

    Geração do pdf

--- lib/report/demanda_report.dart
+++ lib/report/demanda_report.dart
@@ -54,7 +54,6 @@ class _DemandaReportState extends State<DemandaReport> {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -80,78 +79,80 @@ class _DemandaReportState extends State<DemandaReport> {
     final doc = pw.Document(pageMode: PdfPageMode.outlines);
     // final font1 = await PdfGoogleFonts.interRegular();
     // final font2 = await PdfGoogleFonts.interBold();
-    final logo = await rootBundle.loadString('lib/assets/svg/logo-dark.svg');
+    //final logo = await rootBundle.loadString('lib/assets/svg/logo-dark.svg');
+    final logo = pw.MemoryImage(
+      (await rootBundle.load('lib/assets/img/logo-dark-version.png'))
+          .buffer
+          .asUint8List(),
+    );
+
     var font1 = pw.Font.ttf(
         await rootBundle.load("lib/assets/fonts/inter/Inter-Regular.ttf"));
     var font2 = pw.Font.ttf(
         await rootBundle.load("lib/assets/fonts/inter/Inter-ExtraBold.ttf"));
 
-    pw.Row conteudoHorizontal(String tituloAssunto, String conteudo) {
-      return pw.Row(crossAxisAlignment: pw.CrossAxisAlignment.start, children: [
-        pw.Text(tituloAssunto),
-        pw.SizedBox(width: 5),
-        pw.Text(conteudo),
-      ]);
-    }
-
     pw.Column conteudoVertical(String tituloAssunto, String conteudo) {
       return pw.Column(
           crossAxisAlignment: pw.CrossAxisAlignment.start,
           children: [
-            pw.Header(level: 3, text: tituloAssunto),
-            pw.Paragraph(text: conteudo),
+            pw.Text(tituloAssunto,
+                style: pw.TextStyle(font: font2, fontSize: 12)),
+            pw.Paragraph(
+                text: conteudo, style: pw.TextStyle(font: font1, fontSize: 11)),
           ]);
     }
 
     doc.addPage(
       pw.MultiPage(
         pageFormat: PdfPageFormat.a4,
-        theme: pw.ThemeData.withFont(
-          base: font1,
-          bold: font2,
-        ),
+        // theme: pw.ThemeData.withFont(
+        //   base: font1,
+        //   bold: font2,
+        // ),
         //pageFormat: format.copyWith(marginBottom: 1.5 * PdfPageFormat.cm),
         orientation: pw.PageOrientation.portrait,
-        crossAxisAlignment: pw.CrossAxisAlignment.start,
         build: (pw.Context context) => <pw.Widget>[
-          pw.Header(
-              level: 0,
-              title: 'PROPOSTA DE PROJETO',
-              child: pw.Row(
-                  mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: pw.CrossAxisAlignment.center,
-                  children: <pw.Widget>[
-                    pw.Text('PROPOSTA DE PROJETO'),
-                    pw.SvgImage(svg: logo, width: 150),
-                  ])),
+          pw.Center(
+            child: pw.Column(
+                crossAxisAlignment: pw.CrossAxisAlignment.center,
+                mainAxisAlignment: pw.MainAxisAlignment.center,
+                children: [
+                  pw.Image(logo, width: 441, height: 50),
+                  pw.SizedBox(height: 50),
+                  pw.Text('PROPOSTA DE PROJETO',
+                      style: pw.TextStyle(font: font2, fontSize: 15)),
+                ]),
+          ),
+          pw.SizedBox(height: 50),
           conteudoVertical('TÍTULO DA PROPOSTA', titulo),
           conteudoVertical('AUTORES', equipeColaboradores),
           conteudoVertical('DADOS DO PROPONENTE', dadosProponente),
           conteudoVertical('INSTITUIÇÕES ENVOLVIDAS', empresaEnvolvida),
-          conteudoVertical('RESUMO DA PROPOSTA', resumo),
-          conteudoVertical('ÁREA TEMÁTICA', areaTematica),
-          conteudoVertical('JUSTIFICATIVA', propostaConjunto),
-          conteudoVertical('OBJETIVOS', objetivo),
-          conteudoVertical(
-              'RECURSOS DISPONÍVEIS(CONTRAPARTIDA)', contrapartida),
-          conteudoVertical('TEMPO DE EXECUÇÃO(ESTIMADO)', tempo),
-          pw.Header(level: 2, text: 'ANEXOS'),
-          pw.ListView.builder(
-            itemCount: vetAnexos.length,
-            itemBuilder: (context, index) {
-              final url = vetAnexos[index]['file_url'];
+          pw.Container(
+              padding: const pw.EdgeInsets.only(left: 16),
+              child: pw.Column(
+                  crossAxisAlignment: pw.CrossAxisAlignment.start,
+                  children: [
+                    conteudoVertical('1.   RESUMO DA PROPOSTA', resumo),
+                    conteudoVertical('2.   ÁREA TEMÁTICA', areaTematica),
+                    conteudoVertical('3.   JUSTIFICATIVA', propostaConjunto),
+                    conteudoVertical('4.   OBJETIVOS', objetivo),
+                    conteudoVertical('5.   RECURSOS DISPONÍVEIS(CONTRAPARTIDA)',
+                        contrapartida),
+                    conteudoVertical('6. TEMPO DE EXECUÇÃO(ESTIMADO)', tempo),
+                    pw.Text('7.  ANEXOS',
+                        style: pw.TextStyle(font: font2, fontSize: 12)),
+                    pw.ListView.builder(
+                      itemCount: vetAnexos.length,
+                      itemBuilder: (context, index) {
+                        final url = vetAnexos[index]['file_url'];
 
-             return pw.Padding(
-                padding: const pw.EdgeInsets.only(bottom: 10),
-                child: _UrlText(url, url)
-              );
-              //return pw.Bullet(text: url);
-
-              //return pw.Link(destination: url, child: pw.Text('arquivo'));
-
-              // return pw.UrlLink(destination: url, child: pw.Text('Arquivo'));
-            },
-          )
+                        return pw.Padding(
+                            padding: const pw.EdgeInsets.only(bottom: 10),
+                            child: _UrlText(url, url));
+                      },
+                    )
+                  ]))
         ],
       ),
     );
@@ -159,7 +160,6 @@ class _DemandaReportState extends State<DemandaReport> {
     return doc.save();
   }
 }
-
 
 class _UrlText extends pw.StatelessWidget {
   _UrlText(this.text, this.url);
